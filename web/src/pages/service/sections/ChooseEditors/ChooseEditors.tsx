3f7bfb7375cc571d8bd61c7792ba4209ab15d0e5
Eva Kruchinina <kruchinina.eva.i.2.17@gmail.com>
2021-06-07 18:29:31 +0300

    test changes

---
+++ web/src/pages/service/sections/ChooseEditors/ChooseEditors.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import {experimentalStyled as styled} from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
+import Tag from "./icons/Tag";
+import CardEmployerOrService from "../../../../components/Cards/CardEmployerOrService/CardEmployerOrService";
+
+// const Item = styled(Paper)(({theme}) => ({
+//     ...theme.typography.body2,
+//     padding: theme.spacing(1),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+// }));
+
+function ChooseEditors() {
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={11}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} justifyContent="flex-start">
+                            <div style={{display: 'flex', flexDirection: 'row'}}>
+                                <Tag/>
+                                <Typography
+                                    style={{
+                                        fontSize: '24px',
+                                        color: '#000000',
+                                        fontWeight: 400,
+                                        lineHeight: '29.26px',
+                                        marginLeft: '15px',
+                                    }}
+                                >
+                                    Выбор редакции
+                                </Typography>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={11}>
+                    <Grid container>
+                        <Grid item xs={12}
+                        style={{
+                            margin: '15px 15px 15px 0'
+                        }}
+                        >
+                            <CardEmployerOrService/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+
+export default ChooseEditors;
